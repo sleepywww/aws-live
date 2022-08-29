cd7fbde3f11639e663b66c55604b79532007792a
sleepywww <77600351+sleepywww@users.noreply.github.com>
2022-08-29 15:43:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cmytnbij3j7j.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yapjunjia-employee"
+customregion = "us-east-1"
